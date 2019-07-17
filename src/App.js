3b533dddc827d68696e1556161849adb148be80b
Diego Jesús Ubarnes Martínez <diego.ubarnes@sofka.com.co>
2019-07-17 16:02:09 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Sofkianos from './components/Sofkiano';
+import Resultado from './components/resultado';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    termino: '',
+    imagenes: [],
+    pagina: ''
+  }
+  scroll = () => {
+    const elemento = document.querySelector('.jumbotron');
+    elemento.scrollIntoView('smooth', 'start');
+  }
+
+  paginaAnterior = () => {
+    console.log('anterior...');
+    //leer el state de la pag aant
+    let pagina = this.state.pagina;
+    //si la pag es 1 ya no ir hacia atras
+    if (pagina === 1) return null;
+    //restar uno a la pag ant
+    pagina -= 1;
+    //agregar el cambio al state
+    this.setState({
+      pagina
+    }, () => {
+      this.consultarApi();
+      this.scroll();
+    });
+  }
+
+  paginaSiguiente = () => {
+    console.log('siguiente...');
+    //leer el state de la pag act
+    let pagina = this.state.pagina;
+    //sumar uno a la pag act
+    pagina += 1;
+    //agregar el cambio al state
+    this.setState({
+      pagina
+    }, () => {
+      this.consultarApi();
+      this.scroll();
+    });
+  }
+  consultarApi = () => {
+    const termino = this.state.termino;
+    const pagina = this.state.pagina;
+    const url = `https://pixabay.com/api/?key=1732750-d45b5378879d1e877cd1d35a6&q=${termino}&per_page=30&page=${pagina}`;
+
+    fetch(url)
+      .then(respuesta => respuesta.json())
+      .then(resultado => this.setState({ imagenes: resultado.hits }))
+  }
+
+  datosBusqueda = (termino) => {
+    this.setState({
+      termino: termino,
+      pagina: 1
+    }, () => {
+      this.consultarApi();
+    })
+  }
+  render() {
+    return (
+      <div className="app container">
+        <div className="jumbotron">
+          <p className="lead text-center">Buscador De Imagenes</p>
+          <Sofkianos
+            datosBusqueda={this.datosBusqueda}
+          />
+        </div>
+        <div className="row justify-content-center">
+          <Resultado
+            imagenes={this.state.imagenes}
+            paginaAnterior={this.paginaAnterior}
+            paginaSiguiente={this.paginaSiguiente}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
